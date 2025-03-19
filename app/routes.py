2e56823e30e7335f0bc1ad126ca70c3b008db246
Ashton Hall <tennismaster1313@gmail.com>
2025-03-19 23:23:18 +0000

    displayed a match on index page and applied template inheritence for header

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from flask import render_template
 from flask import Blueprint
 
 main = Blueprint('main', __name__)
@@ -5,4 +6,12 @@ main = Blueprint('main', __name__)
 @main.route('/')
 @main.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username':'Ivansia'}
+    matches = [
+        {
+            'opponent':'death knights',
+            'date':'25-03-25 18:00',
+            'location':'Gloucester'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, matches=matches)
