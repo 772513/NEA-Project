a83e9624f39f783a5814607d7e0b99a040c2f989
Ashton Hall <tennismaster1313@gmail.com>
2025-03-21 19:25:51 +0000

    added receiving form data and url_for

--- app/routes.py
+++ app/routes.py
@@ -1,24 +1,32 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from flask import Blueprint
 from app.forms import LoginForm
 
-main = Blueprint('main', __name__)
+main = Blueprint("main", __name__)
 
-@main.route('/')
-@main.route('/index')
+
+@main.route("/")
+@main.route("/index")
 def index():
-    user = {'username':'Ivansia'}
+    user = {"username": "Ivansia"}
     matches = [
         {
-            'opponent':'death knights',
-            'date':'25-03-25 18:00',
-            'location':'Gloucester'
+            "opponent": "death knights",
+            "date": "25-03-25 18:00",
+            "location": "Gloucester",
         }
     ]
-    return render_template('index.html', title='Home', user=user, matches=matches)
+    return render_template("index.html", title="Home", user=user, matches=matches)
 
 
-@main.route('/login')
+@main.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    return render_template('login.html', title='Sign In', form=form)
+    if form.validate_on_submit():
+        flash(
+            "Login requested for user {}, remember_me={}".format(
+                form.username.data, form.remember_me.data
+            )
+        )
+        return redirect(url_for('main.index'))
+    return render_template("login.html", title="Sign In", form=form)
